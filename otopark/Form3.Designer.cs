21196b8e6221fcf10f1907d76152c3742f1c646f
MuhammetEfeCengiz <92029662+MuhammetEfeCengiz@users.noreply.github.com>
2022-01-12 00:53:19 +0300

    Add project files.

---
+++ otopark/Form3.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace otopark
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.Aboneno_lbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.guncelle_btn = new System.Windows.Forms.Button();
+            this.Kaydet_btn = new System.Windows.Forms.Button();
+            this.Sorgulla_btn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Aboneno_lbl
+            // 
+            this.Aboneno_lbl.AutoSize = true;
+            this.Aboneno_lbl.BackColor = System.Drawing.Color.Transparent;
+            this.Aboneno_lbl.Location = new System.Drawing.Point(154, 123);
+            this.Aboneno_lbl.Name = "Aboneno_lbl";
+            this.Aboneno_lbl.Size = new System.Drawing.Size(31, 13);
+            this.Aboneno_lbl.TabIndex = 0;
+            this.Aboneno_lbl.Text = "1001";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(87, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ali";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(113, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ak";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(122, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "34 AA 5555";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(166, 325);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "01.01.2022";
+            // 
+            // guncelle_btn
+            // 
+            this.guncelle_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("guncelle_btn.BackgroundImage")));
+            this.guncelle_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.guncelle_btn.Location = new System.Drawing.Point(41, 376);
+            this.guncelle_btn.Name = "guncelle_btn";
+            this.guncelle_btn.Size = new System.Drawing.Size(129, 40);
+            this.guncelle_btn.TabIndex = 5;
+            this.guncelle_btn.UseVisualStyleBackColor = true;
+            // 
+            // Kaydet_btn
+            // 
+            this.Kaydet_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Kaydet_btn.BackgroundImage")));
+            this.Kaydet_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Kaydet_btn.Location = new System.Drawing.Point(214, 376);
+            this.Kaydet_btn.Name = "Kaydet_btn";
+            this.Kaydet_btn.Size = new System.Drawing.Size(140, 40);
+            this.Kaydet_btn.TabIndex = 6;
+            this.Kaydet_btn.UseVisualStyleBackColor = true;
+            // 
+            // Sorgulla_btn
+            // 
+            this.Sorgulla_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Sorgulla_btn.BackgroundImage")));
+            this.Sorgulla_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Sorgulla_btn.Location = new System.Drawing.Point(413, 138);
+            this.Sorgulla_btn.Name = "Sorgulla_btn";
+            this.Sorgulla_btn.Size = new System.Drawing.Size(133, 32);
+            this.Sorgulla_btn.TabIndex = 7;
+            this.Sorgulla_btn.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Gray;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(422, 195);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(299, 208);
+            this.listBox1.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Gray;
+            this.textBox1.Location = new System.Drawing.Point(524, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "Abone No";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(763, 428);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Sorgulla_btn);
+            this.Controls.Add(this.Kaydet_btn);
+            this.Controls.Add(this.guncelle_btn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Aboneno_lbl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Aboneno_lbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button guncelle_btn;
+        private System.Windows.Forms.Button Kaydet_btn;
+        private System.Windows.Forms.Button Sorgulla_btn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
